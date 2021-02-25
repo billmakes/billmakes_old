a152166fd696b803bab260f9b6ff6eb6e1503310
Bill Hilbert <hilbertwilliam@gmail.com>
2021-02-25 11:12:45 -0500

    test

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicDir: process.env.NODE_ENV === 'production' ? '/billmakes/' : '/',
+  publicDir:
+    process.env.NODE_ENV === 'production' ? '/billmakes/' : '/billmakes/',
   plugins: [vue()]
 })
