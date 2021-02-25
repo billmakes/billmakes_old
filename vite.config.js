491bd7ff05f1171d282720643d4ae6d6adfcd4eb
Bill Hilbert <hilbertwilliam@gmail.com>
2021-02-25 10:56:56 -0500

    deploy settings

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicDir: process.env.NODE_ENV === 'production' ? '/billmakes/' : '/',
   plugins: [vue()]
 })
