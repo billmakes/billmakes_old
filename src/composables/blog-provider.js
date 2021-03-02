361db85c38292b880515ad3d5b4e2fe52f62f1c7
Bill Hilbert <hilbertwilliam@gmail.com>
2021-03-02 15:48:43 -0500

    test

--- src/composables/blog-provider.js
+++ src/composables/blog-provider.js
@@ -1,6 +1,6 @@
 import { reactive, computed, readonly } from 'vue'
 
-const posts = import.meta.glob('../../blog/*.md')
+const posts = import.meta.glob('../../public/assets/blog/*.md')
 
 for (const path in posts) {
   let newPath = path
@@ -9,21 +9,23 @@ for (const path in posts) {
     .slice(0, -1)
     .join('.')
 
-  import('../../blog/' + newPath + '.md' + '?raw').then(async post => {
-    let regMetaMatch = post.default.match(/(?<=\%)(.*?)(?=\%)/)[1]
-    let meta = await JSON.parse(regMetaMatch)
+  import('../../public/assets/blog/' + newPath + '.md' + '?raw').then(
+    async post => {
+      let regMetaMatch = post.default.match(/(?<=\%)(.*?)(?=\%)/)[1]
+      let meta = await JSON.parse(regMetaMatch)
 
-    if (!meta) {
-      return console.error(
-        'Error: please include a metadata line at the top of your markdown file. (ex: % {"title": "test", "date": "today"} % )'
-      )
+      if (!meta) {
+        return console.error(
+          'Error: please include a metadata line at the top of your markdown file. (ex: % {"title": "test", "date": "today"} % )'
+        )
+      }
+
+      state.posts.push({
+        meta,
+        post: post.default.replace(`%${regMetaMatch}%`, '')
+      })
     }
-
-    state.posts.push({
-      meta,
-      post: post.default.replace(`%${regMetaMatch}%`, '')
-    })
-  })
+  )
 }
 
 const state = reactive({
